bc7d3a614b3c410713e8f7a826ffa8f164870b1c
Ankoay-Feno <ankoayfeno@gmail.com>
2026-02-04 09:51:52 +0300

    infrastructure

---
+++ lambda-function/terraform.tfvars
@@ -0,0 +1,1 @@
+aws_profile="ankoay-tnm"
---
+++ lambda-function/terraform/main.tf
@@ -0,0 +1,116 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  profile = var.aws_profile
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.project_name}-lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = { Service = "lambda.amazonaws.com" }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_basic" {
+  name = "${var.project_name}-lambda-basic-logs"
+  role = aws_iam_role.lambda_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+resource "null_resource" "build" {
+  triggers = {
+    app_hash = sha256(join("", [
+      for f in sort(fileset("${path.module}/../src/app", "**")) :
+      filesha256("${path.module}/../src/app/${f}")
+    ]))
+    req_hash = filesha256("${path.module}/../src/requirements.txt")
+  }
+
+  provisioner "local-exec" {
+    command = "${path.module}/build.sh"
+  }
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/build"
+  output_path = "${path.module}/build/lambda.zip"
+
+  depends_on = [null_resource.build]
+}
+
+resource "aws_lambda_function" "api" {
+  function_name = "${var.project_name}-api"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "app.main.handler"
+  runtime       = "python3.11"
+  filename      = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  depends_on = [aws_iam_role_policy.lambda_basic]
+}
+
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "${var.project_name}-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.api.arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
+
+output "api_url" {
+  value = aws_apigatewayv2_api.http_api.api_endpoint
+}
